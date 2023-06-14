e915b9470cddaa34cabd6008bf51c037a6dbb7a8
Mário Lucas <mariolucasdev@gmail.com>
2023-06-14 16:51:47 -0300

    adding ans settings up husky and pre-commit

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,16 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
+    <header>
+        <div class="max-w-7xl mx-auto">
+            <nav>
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+            </nav>
+        </div>
+    </header>
 
-    <div class="max-w-7xl mx-auto">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <main class="max-w-7xl mx-auto min-h-screen">
-    <RouterView />
-  </main>
-
+    <main class="max-w-7xl mx-auto min-h-screen">
+        <RouterView />
+    </main>
 </template>
