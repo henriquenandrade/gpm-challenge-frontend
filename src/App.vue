ef083c81e2f10bf0c9f2376ffec51003a75a47ef
Mário Lucas <mariolucasdev@gmail.com>
2023-06-14 22:50:36 -0300

    adding delete appliance by api

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,14 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
     <header>
         <div class="max-w-7xl mx-auto">
-            <nav class="p-5">
-                <RouterLink class="px-5 py-3 mr-4 bg-indigo-500 hover:bg-indigo-600 rounded text-white" to="/">Home</RouterLink>
-                <RouterLink class="px-5 py-3 mr-4 bg-indigo-500 hover:bg-indigo-600 rounded text-white" to="/appliances">Eletrodomésticos</RouterLink>
+
+            <div class=" p-3 text-center ">
+                <h1 class=" text-4xl font-bold text-indigo-800 p-2 mx-auto w-full "> GPM - Appliances </h1>
+            </div>
+
+            <nav class="p-5 text-center">
+                <RouterLink class="px-5 py-3 mr-4 bg-indigo-500 hover:bg-indigo-600 rounded-lg text-white" to="/">Home</RouterLink>
+                <RouterLink class="px-5 py-3 mr-4 bg-indigo-500 hover:bg-indigo-600 rounded-lg text-white" to="/appliances">Eletrodomésticos</RouterLink>
             </nav>
         </div>
     </header>
